ea8b01b55a8801d9bb1f915fd68403d76bb9daa7
Dong060 <bdong0610@gmail.com>
2023-10-08 20:49:56 +0700

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Vi_dien_tu_E_Wallet
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vi_dien_tu_E_Wallet.MvcApplication" Language="C#" %>
